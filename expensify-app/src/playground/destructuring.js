608c401364a63f357eef12909cc09ede28b195ab
Masato Takahashi <masato.takaha@gmail.com>
2017-11-04 11:35:58 -0700

    array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,8 @@
+
+//////
+//Object Destructuring
+/////
+
 // const person = {
 //   name: "Masato",
 //   age: 32,
@@ -16,14 +21,26 @@
 //   console.log(`It's ${temperature} in ${city}.`)
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+// const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName); //if no name, default to sefl-published
+// console.log(publisherName); //if no name, default to sefl-published
+
+///////
+//Array Destructuring
+/////// 
+
+// const address = ['123 Abc Lane', 'Sunnyvale', 'California', '12345'];
+// const [, city, state = 'Oregon'] = address;
+// console.log(`You are in ${city}, ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`)
